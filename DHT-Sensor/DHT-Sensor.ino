8cbf0d0427eef32114d9e9bddb923c9ccdd3b1a5
M.Schwarz <Michel_Schwarz@ymail.com>
2020-09-03 21:44:30 +0200

    feat: added CO Sensor settings

--- DHT-Sensor/DHT-Sensor.ino
+++ DHT-Sensor/DHT-Sensor.ino
@@ -4,8 +4,8 @@
 #include <DHT.h>
 
 // Set your AppEUI and AppKey
-const char *appEui = "#############################";
-const char *appKey = "#############################";
+const char *appEui = "XXXXXXXXXXXXXXX";
+const char *appKey = "XXXXXXXXXXXXXXX";
 
 #define loraSerial Serial1
 #define debugSerial Serial
@@ -22,6 +22,29 @@ const char *appKey = "#############################";
 DHT dht(DHTPIN, DHTTYPE);
 
 TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
+
+float coSensorMeasure(){
+    float sensor_volt;
+    float RS_gas; // Get value of RS in a GAS
+    float ratio; // Get ratio RS_GAS/RS_air
+    int sensorValue = analogRead(A0);
+    sensor_volt=(float)sensorValue/1024*5.0;
+    RS_gas = (5.0-sensor_volt)/sensor_volt; // omit *RL
+
+    /*-Replace the name "R0" with the value of R0 in the demo of First Test -*/
+    ratio = RS_gas/0.08;  // ratio = RS/R0
+     /*-----------------------------------------------------------------------*/
+    Serial.print("sensor_volt = ");
+    Serial.println(sensor_volt);
+    Serial.print("RS_ratio = ");
+    Serial.println(RS_gas);
+    Serial.print("Rs/R0 = ");
+    Serial.println(ratio);
+    Serial.print("\n\n");
+    delay(1000);
+
+    return RS_gas;
+}
 
 void setup()
 {
@@ -53,16 +76,24 @@ void loop()
   uint16_t temperature = dht.readTemperature(false) * 100;
 
   // Split both words (16 bits) into 2 bytes of 8
-  byte payload[4];
-  payload[0] = highByte(temperature);
-  payload[1] = lowByte(temperature);
-  payload[2] = highByte(humidity);
-  payload[3] = lowByte(humidity);
-
+  uint16_t co = coSensorMeasure()*100;
+  //uint16_t co = 5;
+  
+  debugSerial.print("CO: ");
+  debugSerial.println(co);
+  
   debugSerial.print("Temperature: ");
   debugSerial.println(temperature);
   debugSerial.print("Humidity: ");
   debugSerial.println(humidity);
+  
+  byte payload[6];
+  payload[0] = highByte(temperature);
+  payload[1] = lowByte(temperature);
+  payload[2] = highByte(humidity);
+  payload[3] = lowByte(humidity);
+  payload[4] = highByte(co);
+  payload[5] = lowByte(co);
 
   ttn.sendBytes(payload, sizeof(payload));
 }
